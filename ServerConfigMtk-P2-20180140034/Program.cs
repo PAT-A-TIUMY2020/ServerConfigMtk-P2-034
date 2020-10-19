cb8707467fb9f39cca4e252288210da2a951c59d
Aalfinuraziz <alfinuraziz885@gmail.com>
2020-10-19 18:52:02 +0700

    Add project files.

---
+++ ServerConfigMtk-P2-20180140034/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMtk_P1_20180140034;
+
+namespace ServerConfigMtk_P2_20180140034
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server Sudah Siap!!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
